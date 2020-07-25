c0083bf04af8054a44c7cd9382a7f9ec1791f4b9
Kerndog73 <kerndog73@gmail.com>
2020-07-25 11:00:03 +0930

    Add ordered pixel formats

--- include/Graphics/format.hpp
+++ include/Graphics/format.hpp
@@ -13,22 +13,40 @@
 
 namespace gfx {
 
-// TODO: fixed order pixel formats
-
-struct ABGR {
+template <auto Ch0, auto Ch1, auto Ch2, auto Ch3>
+struct GenericRGBA {
   using Pixel = std::uint32_t;
   
+  static_assert(Ch0 != Ch1 && Ch0 != Ch2 && Ch0 != Ch3);
+  static_assert(Ch1 != Ch2 && Ch1 != Ch3);
+  static_assert(Ch2 != Ch3);
+
+  static constexpr Color color(const Pixel pixel) noexcept {
+    Color color{0, 0, 0, 0};
+    (color.*Ch0) = pixel >> 24;
+    (color.*Ch1) = (pixel >> 16) & 0xFF;
+    (color.*Ch2) = (pixel >> 8) & 0xFF;
+    (color.*Ch3) = pixel & 0xFF;
+    return color;
+  }
+  static constexpr Pixel pixel(const Color color) noexcept {
+    return (Pixel{color.*Ch0} << 24) |
+           (Pixel{color.*Ch1} << 16) |
+           (Pixel{color.*Ch2} <<  8) |
+            Pixel{color.*Ch3};
+  }
+  
   static constexpr std::uint8_t red(const Pixel pixel) noexcept {
-    return pixel & 0xFF;
+    return color(pixel).r;
   }
   static constexpr std::uint8_t green(const Pixel pixel) noexcept {
-    return (pixel >> 8) & 0xFF;
+    return color(pixel).g;
   }
   static constexpr std::uint8_t blue(const Pixel pixel) noexcept {
-    return (pixel >> 16) & 0xFF;
+    return color(pixel).b;
   }
   static constexpr std::uint8_t alpha(const Pixel pixel) noexcept {
-    return pixel >> 24;
+    return color(pixel).a;
   }
   
   static constexpr Pixel pixel(const int r, const int g, const int b, const int a = 255) noexcept {
@@ -39,53 +57,19 @@ struct ABGR {
       static_cast<std::uint8_t>(a)
     });
   }
-  
-  static constexpr Color color(const Pixel pixel) noexcept {
-    return {red(pixel), green(pixel), blue(pixel), alpha(pixel)};
-  }
-  static constexpr Pixel pixel(const Color color) noexcept {
-    return (Pixel{color.a} << 24) |
-           (Pixel{color.b} << 16) |
-           (Pixel{color.g} <<  8) |
-            Pixel{color.r};
-  }
 };
 
-struct ARGB {
-  using Pixel = std::uint32_t;
+#define GFX_GENERIC_RGBA(CH0, CH1, CH2, CH3)                                    \
+  ::gfx::GenericRGBA<                                                           \
+    &::gfx::Color::CH0,                                                         \
+    &::gfx::Color::CH1,                                                         \
+    &::gfx::Color::CH2,                                                         \
+    &::gfx::Color::CH3                                                          \
+  >
 
-  static constexpr std::uint8_t red(const Pixel pixel) noexcept {
-    return (pixel >> 16) & 0xFF;
-  }
-  static constexpr std::uint8_t green(const Pixel pixel) noexcept {
-    return (pixel >> 8) & 0xFF;
-  }
-  static constexpr std::uint8_t blue(const Pixel pixel) noexcept {
-    return pixel & 0xFF;
-  }
-  static constexpr std::uint8_t alpha(const Pixel pixel) noexcept {
-    return pixel >> 24;
-  }
-  
-  static constexpr Pixel pixel(const int r, const int g, const int b, const int a = 255) noexcept {
-    return pixel({
-      static_cast<std::uint8_t>(r),
-      static_cast<std::uint8_t>(g),
-      static_cast<std::uint8_t>(b),
-      static_cast<std::uint8_t>(a)
-    });
-  }
-
-  static constexpr Color color(const Pixel pixel) noexcept {
-    return {red(pixel), green(pixel), blue(pixel), alpha(pixel)};
-  }
-  static constexpr Pixel pixel(const Color color) noexcept {
-    return (Pixel{color.a} << 24) |
-           (Pixel{color.r} << 16) |
-           (Pixel{color.g} <<  8) |
-            Pixel{color.b};
-  }
-};
+using ARGB = GFX_GENERIC_RGBA(a, r, g, b);
+using RGBA_LE = GFX_GENERIC_RGBA(a, b, g, r);
+using RGBA_BE = GFX_GENERIC_RGBA(r, g, b, a);
 
 template <typename Target = ARGB>
 struct I {
@@ -99,28 +83,44 @@ struct I {
   }
 };
 
-struct YA {
+template <auto Ch0, auto Ch1>
+struct GenericYA {
   using Pixel = std::uint16_t;
-
+  
+  static_assert(Ch0 != Ch1);
+  static_assert(Ch0 == &Color::r || Ch0 == &Color::a);
+  static_assert(Ch1 == &Color::r || Ch1 == &Color::a);
+  
+  static constexpr Color color(const Pixel pixel) noexcept {
+    Color color{0, 0, 0, 0};
+    (color.*Ch0) = pixel >> 8;
+    (color.*Ch1) = pixel & 0xFF;
+    color.g = color.r;
+    color.b = color.r;
+    return color;
+  }
+  static constexpr Pixel pixel(const Color color) noexcept {
+    return (Pixel{color.*Ch0} << 8) | Pixel{color.*Ch1};
+  }
+  
   static constexpr std::uint8_t gray(const Pixel pixel) noexcept {
-    return pixel & 255;
+    return color(pixel).r;
   }
   static constexpr std::uint8_t alpha(const Pixel pixel) noexcept {
-    return pixel >> 8;
+    return color(pixel).a;
   }
-  static constexpr Pixel pixel(const int gray, const int alpha) noexcept {
+  
+  static constexpr Pixel pixel(const int gray, const int alpha = 255) noexcept {
     return pixel({
       static_cast<std::uint8_t>(gray), 0, 0, static_cast<std::uint8_t>(alpha)
     });
   }
-
-  static constexpr Color color(const Pixel pixel) noexcept {
-    return {gray(pixel), gray(pixel), gray(pixel), alpha(pixel)};
-  }
-  static constexpr Pixel pixel(const Color color) noexcept {
-    return (color.a << 8) | color.r;
-  }
 };
+
+using AY = GenericYA<&Color::a, &Color::r>;
+using YA = GenericYA<&Color::r, &Color::a>;
+using YA_LE = AY;
+using YA_BE = YA;
 
 struct Y {
   using Pixel = std::uint8_t;
